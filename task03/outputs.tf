98f2e6d422858774fb718f559bb53d8354361a14
Vedant Awasthi <vedant_awasthi@epam.com>
2026-04-15 08:47:17 +0000

    fixed descriptions on output.tf

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,14 +1,14 @@
-output "resource_group_id" {
+output "rg_id" {
   value       = azurerm_resource_group.rg.id
   description = "ID of the resource group"
 }
 
-output "storage_account_sa_blob_endpoint" {
+output "sa_blob_endpoint" {
   value       = azurerm_storage_account.sa.primary_blob_endpoint
   description = "Primary blob endpoint of the storage account"
 }
 
-output "virtual_network_id" {
+output "vnet_id" {
   value       = azurerm_virtual_network.vnet.id
   description = "ID of the virtual network"
 }
